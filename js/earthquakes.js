2d3c78ca0a557585cfa62d70b322f36dae395142
Marissa Ho <mewhho@gmail.com>
2013-11-11 23:29:41 -0800

    lab 7

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -1,3 +1,9 @@
+/*
+Marissa Ho
+INFO 343
+Lab 7
+*/
+
 /* earthquakes.js
     Script file for the INFO 343 Lab 7 Earthquake plotting page
 
@@ -29,4 +35,71 @@ gov.usgs.quakesMap;
 $(document).ajaxError(function(event, jqXHR, err){
     alert('Problem obtaining data: ' + jqXHR.statusText);
 });
+
+$(function(){
+	getQuakes();
+});
+
+//getQuakes()
+//queries the server for the list of recent quakes
+//and plots them on a Google map
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes){
+	    gov.usgs.quakes = quakes;
+		$('.message').append('Displaying ' + quakes.length + ' earthquakes');
+	    gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+		    center: new google.maps.LatLng(0,0),        //centered on 0/0
+		    zoom: 2,                                    //zoom level 2
+		    mapTypeId: google.maps.MapTypeId.TERRAIN,   //terrain map
+		    streetViewControl: false                    //no street view
+		});
+	    addQuakeMarkers(quakes, gov.usgs.quakesMap);
+	}); //handle returned data function
+} //getQuakes()
+
+//addQuakeMarkers()
+//parameters
+// - quakes (array) array of quake data objects
+// - map (google.maps.Map) Google map we can add markers to
+// no return value
+function addQuakeMarkers(quakes, map) {
+    
+    //loop over the quakes array and add a marker for each quake
+    var quake;      //current quake data
+    var idx;        //loop counter
+    var infoWindow; //InfoWindow for quake
+
+    for (idx = 0; idx < quakes.length; ++idx) {
+        quake = quakes[idx];
+        //latitude of current quake = quake.location.latitude 
+        //longitutde of current quake = quake.location.longitude
+
+        if (quake.location) { 
+        	//assuming that the variable 'quake' is set to 
+			//the current quake object within the quakes array...
+			quake.mapMarker = new google.maps.Marker({
+			    map: map,
+			    position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+			});   
+        }
+        infoWindow = new google.maps.InfoWindow({
+		    content: new Date(quake.datetime).toLocaleString() + 
+	            ': magnitude ' + quake.magnitude + ' at depth of ' + 
+	            quake.depth + ' meters'
+		});
+		registerInfoWindow(map, quake.mapMarker, infoWindow);
+    }
+} //addQuakeMarkers()
+
+
+function registerInfoWindow(map, marker, infoWindow) {
+    google.maps.event.addListener(marker, 'click', function(){
+    	if (gov.usgs.iw) {
+    		gov.usgs.iw.close();
+    	}
+    	gov.usgs.iw = infoWindow;
+        infoWindow.open(map, marker);
+
+    });                
+} //registerInfoWindow()
 
